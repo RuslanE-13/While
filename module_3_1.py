91490c1134b38e81fd2c5002e99b13f373f1c4bb
RuslanE-13 <enikeev_93@mail.ru>
2024-10-25 00:06:20 +0500

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,24 @@
+# Пункты задачи:
+# Создать переменную calls = 0 вне функций.
+# Создать функцию count_calls и изменять в ней значение переменной calls. Эта функция должна вызываться в остальных двух функциях.
+# Создать функцию string_info с параметром string и реализовать логику работы по описанию.
+# Создать функцию is_contains с двумя параметрами string и list_to_search, реализовать логику работы по описанию.
+# Вызвать соответствующие функции string_info и is_contains произвольное кол-во раз с произвольными данными.
+# Вывести значение переменной calls на экран(в консоль).
+
+
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+def is_contains(string, strings):
+    count_calls()
+    return string.lower() in map(str.lower, strings)
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
